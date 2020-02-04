d7fde34b31b39ba59c9c8c9537bb17126dc54dee
Jeff Tab <j.tab.jr@gmail.com>
2020-02-04 16:06:19 -0500

    working on getting responses to workable format

--- index.js
+++ index.js
@@ -8,50 +8,63 @@ inquirer
             message: "What is your GitHub username?",
             name: "username",
         },
-        // {
-        //     type: "input",
-        //     message: "What is your email?",
-        //     name: "email"
-        // },
-        // {
-        //     type: "input",
-        //     message: "What is the project title?",
-        //     name: "title"
-        // },
-        // {
-        //     type: "input",
-        //     message: "What is a description of the project?",
-        //     name: "description"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How does this get installed?",
-        //     name: "installation"
-        // },
-        // {
-        //     type: "input",
-        //     message: "How does this get used?",
-        //     name: "usage"
-        // },
-        // {
-        //     type: "checkbox",
-        //     message: "Select the license(s) used:",
-        //     name: "license",
-        //     choices: [
-        //         { name: 'Apache License 2.0' },
-        //         { name: 'GNU GPLv3' },
-        //         { name: 'MIT' },
-        //         { name: 'ISC' }
-        //     ]
-        // }
+        {
+            type: "input",
+            message: "What is your email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the project title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is a description of the project?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "How does this get installed?",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "How does this get used?",
+            name: "usage"
+        },
+        {
+            type: "checkbox",
+            message: "Select the license(s) used:",
+            name: "license",
+            choices: [
+                { name: 'Apache License 2.0' },
+                { name: 'GNU GPLv3' },
+                { name: 'MIT' },
+                { name: 'ISC' }
+            ]
+        }
     ])
-    .then(({ username }) => {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        return axios.get(queryUrl);
-    })
-    .then(({ data }) => {
+    .then((response) => {
+        console.log(response);
+        const list = Object.values(response);
+        console.log(list);
+        const mappedItems = list.map(markup);
+        function markup(item) {
+            return (`## ${item}`)
+        };
+        console.log(mappedItems);
 
+        console.log(readMeInformation);
+        return writeFile("ReadMe.md", readMeInformation)
     })
+    // .then((username) => {
+    //     const queryUrl = `https://api.github.com/users/${username}`;
+    //     return axios.get(queryUrl);
+    // })
+    // .then(({ data }) => {
+    //     console.log(data);
+    // })
     .then(res => {
         console.log(res);
     })
